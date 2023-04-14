aad49d973def0da7ba58357b38e81e8435622f16
Donald Ebube <donaldebube@yahoo.com>
2023-04-14 16:40:48 +0100

    New Commit

---
+++ Naija_Houses.sql
@@ -0,0 +1,18 @@
+SELECT *
+FROM NaijaHouses
+
+-- Restructure each column title to a format that you understand
+EXEC sp_rename 'NaijaHouses.bedrooms', 'Bedrooms', 'COLUMN';
+EXEC sp_rename 'NaijaHouses.bathrooms', 'Bathrooms', 'COLUMN';
+EXEC sp_rename 'NaijaHouses.toilets', 'Toilets', 'COLUMN';
+EXEC sp_rename 'NaijaHouses.parking_space', 'Parking Space', 'COLUMN';
+EXEC sp_rename 'NaijaHouses.title', 'Title', 'COLUMN';
+EXEC sp_rename 'NaijaHouses.town', 'Town', 'COLUMN';
+EXEC sp_rename 'NaijaHouses.state', 'State', 'COLUMN';
+EXEC sp_rename 'NaijaHouses.price', 'Price', 'COLUMN';
+
+SELECT TOP 10 *
+FROM NaijaHouses
+
+SELECT COUNT(DISTINCT bedrooms) AS 'Number of Bedrooms'
+FROM NaijaHouses
